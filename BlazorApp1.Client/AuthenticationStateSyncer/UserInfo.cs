5d2ed30a1e41484a20892e9fd4fd473fac050413
274188A <40014477+274188A@users.noreply.github.com>
2025-01-02 13:20:21 +0800

    added project files

---
+++ BlazorApp1.Client/AuthenticationStateSyncer/UserInfo.cs
@@ -0,0 +1,6 @@
+﻿public class UserInfo
+{
+  public required string UserId { get; set; }
+  public required string Name { get; set; }
+  public required string Email { get; set; }
+}
---
+++ BlazorApp1/AuthenticationStateSyncer/UserInfo.cs
@@ -0,0 +1,8 @@
+﻿namespace AuthenticationStateSyncer;
+
+public class UserInfo
+{
+  public required string UserId { get; set; }
+  public required string Name { get; set; }
+  public required string Email { get; set; }
+}
